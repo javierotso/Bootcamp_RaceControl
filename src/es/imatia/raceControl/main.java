522f1a3b2b63e8edbd8e721f40d2708254c8bcd1
javierotso <85392399+javierotso@users.noreply.github.com>
2022-09-15 12:16:05 +0200

    Test commit

--- src/es/imatia/raceControl/main.java
+++ src/es/imatia/raceControl/main.java
@@ -60,38 +60,37 @@ public class main {
 		oos.writeObject(garageList);
 		oos.writeObject(competitionList);
 		oos.close();
-	}
 
-	/*
-	 * En caso de perder los datos: descomentar esto: Garage escuderia1 = new
-	 * Garage("Escuderia 1"); Garage escuderia2 = new Garage("Escuderia 2");
-	 * 
-	 * Car c1e1 = new Car("Coche", "1", escuderia1); Car c2e1 = new Car("Coche",
-	 * "2", escuderia1); Car c3e1 = new Car("Coche", "3", escuderia1); Car c4e1 =
-	 * new Car("Coche", "4", escuderia1);
-	 * 
-	 * Car c1e2 = new Car("Coche", "1", escuderia2); Car c2e2 = new Car("Coche",
-	 * "2", escuderia2); Car c3e2 = new Car("Coche", "3", escuderia2); Car c4e2 =
-	 * new Car("Coche", "4", escuderia2); Car c5e2 = new Car("Coche", "5",
-	 * escuderia2);
-	 * 
-	 * escuderia1.addCar(c1e1); escuderia1.addCar(c2e1); escuderia1.addCar(c3e1);
-	 * escuderia1.addCar(c4e1);
-	 * 
-	 * escuderia2.addCar(c1e2); escuderia2.addCar(c2e2); escuderia2.addCar(c3e2);
-	 * escuderia2.addCar(c4e2); escuderia2.addCar(c5e2);
-	 * 
-	 * garageList.put(escuderia1.getGarageName(), escuderia1);
-	 * garageList.put(escuderia2.getGarageName(), escuderia2);
-	 * 
-	 * Competition torneo1 = new Competition("Torneo 1", 3);
-	 * competitionList.put(torneo1.getCompetitionName(), torneo1);
-	 * 
-	 * Race c1t1 = new StandardRace("Carrera 1", escuderia2); Race c2t1 = new
-	 * EliminationRace("Carrera 2", escuderia1);
-	 * 
-	 * Race c3t1 = new StandardRace("Carrera 3", garageList);
-	 * 
-	 * torneo1.addRace(c3t1); torneo1.addRace(c1t1); torneo1.addRace(c2t1);
-	 */
+		/* En caso de perder los datos: descomentar esto: */
+		/*
+		 * Garage escuderia1 = new Garage("Escuderia 1"); Garage escuderia2 = new
+		 * Garage("Escuderia 2");
+		 * 
+		 * Car c1e1 = new Car("Coche", "1", escuderia1); Car c2e1 = new Car("Coche",
+		 * "2", escuderia1); Car c3e1 = new Car("Coche", "3", escuderia1); Car c4e1 =
+		 * new Car("Coche", "4", escuderia1); Car c1e2 = new Car("Coche", "1",
+		 * escuderia2); Car c2e2 = new Car("Coche", "2", escuderia2); Car c3e2 = new
+		 * Car("Coche", "3", escuderia2); Car c4e2 = new Car("Coche", "4", escuderia2);
+		 * Car c5e2 = new Car("Coche", "5", escuderia2);
+		 * 
+		 * escuderia1.addCar(c1e1); escuderia1.addCar(c2e1); escuderia1.addCar(c3e1);
+		 * escuderia1.addCar(c4e1);
+		 * 
+		 * escuderia2.addCar(c1e2); escuderia2.addCar(c2e2); escuderia2.addCar(c3e2);
+		 * escuderia2.addCar(c4e2); escuderia2.addCar(c5e2);
+		 * 
+		 * garageList.put(escuderia1.getGarageName(), escuderia1);
+		 * garageList.put(escuderia2.getGarageName(), escuderia2);
+		 * 
+		 * Competition torneo1 = new Competition("Torneo 1", 3);
+		 * competitionList.put(torneo1.getCompetitionName(), torneo1);
+		 * 
+		 * Race c1t1 = new StandardRace("Carrera 1", escuderia2); Race c2t1 = new
+		 * EliminationRace("Carrera 2", escuderia1);
+		 * 
+		 * Race c3t1 = new StandardRace("Carrera 3", garageList);
+		 * 
+		 * torneo1.addRace(c3t1); torneo1.addRace(c1t1); torneo1.addRace(c2t1);
+		 */
+	}
 }
